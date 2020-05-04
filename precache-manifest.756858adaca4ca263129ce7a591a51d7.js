0147b7191b3b63853683894c5f6cb632dc8fa2cd
cwalbanese <631gti@gmail.com>
2020-05-04 15:08:52 -0400

    Updates

---
+++ precache-manifest.756858adaca4ca263129ce7a591a51d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "783d8ebe9b8db2c6e67740a03c8645ac",
+    "url": "/covid-19-test/index.html"
+  },
+  {
+    "revision": "d986a2f000186b0a994d",
+    "url": "/covid-19-test/static/css/main.c56c21dd.chunk.css"
+  },
+  {
+    "revision": "c7f0d57f69af8326a382",
+    "url": "/covid-19-test/static/js/2.d0322420.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid-19-test/static/js/2.d0322420.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d986a2f000186b0a994d",
+    "url": "/covid-19-test/static/js/main.7596e8a6.chunk.js"
+  },
+  {
+    "revision": "7b25f8b8ff6e65daf135",
+    "url": "/covid-19-test/static/js/runtime-main.6a91be38.js"
+  }
+]);
